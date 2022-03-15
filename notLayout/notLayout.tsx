b97db1a64d87d96be0e9b3278d805e6ecd0cced8
Chuck <fanck0605@qq.com>
2022-03-15 21:33:24 +0800

    Update notLayout.tsx

--- notLayout/notLayout.tsx
+++ notLayout/notLayout.tsx
@@ -6,6 +6,9 @@ import type { match } from 'react-router';
  * 当路径精确匹配时, 会展示这个页面组件.
  * 当路径匹配到子路由时, 会直接展示子路由组件.
  *
+ * @author Chuck Fan
+ * @version 2022-03-15
+ *
  * @see https://zh-hans.reactjs.org/docs/higher-order-components.html
  * @see https://umijs.org/zh-CN/docs/routing#routes
  * @see https://umijs.org/zh-CN/docs/routing#%E8%B7%AF%E7%94%B1%E7%BB%84%E4%BB%B6%E5%8F%82%E6%95%B0
